787a45b30efdc5305740d677644e3667a56a5792
Ali Churcher <churcher.ali@gmail.com>
2016-10-30 15:20:49 +0100

    Moved hardcoded data into state for when it really is stateful

--- public/src/app.jsx
+++ public/src/app.jsx
@@ -1,15 +1,10 @@
 const React = require('react');
 const HealthDataBox = require('./health-data-box.jsx');
-const HardCodedHeartData = [
-    { time: '14:00', BPM: '76' },
-    { time: '5:59', BPM: '14' },
-    { time: '8:22', BPM: '25' },
-];
 
 class App extends React.Component {
   render () {
     return (
-      <HealthDataBox heartData={HardCodedHeartData}/>
+        <HealthDataBox />
     );
   }
 }
--- public/src/heart-rate-table.jsx
+++ public/src/heart-rate-table.jsx
@@ -2,9 +2,19 @@ const React = require('react');
 const HeartRateRow = require('./heart-rate-row.jsx');
 
 class HeartRateTable extends React.Component {
+  constructor (props) {
+    super(props);
+    const HardCodedHeartData = [
+        { time: '14:00', BPM: '76' },
+        { time: '5:59', BPM: '14' },
+        { time: '8:22', BPM: '25' }
+    ];
+    this.state = {heartData: HardCodedHeartData};
+  }
+
   render () {
     const rows = [];
-    rows.push(this.props.heartData.map((record) => <HeartRateRow key={record.time} heartRecord={record} />));
+    rows.push(this.state.heartData.map((record) => <HeartRateRow key={record.time} heartRecord={record} />));
     return (
       <div>
         Time   BPM
