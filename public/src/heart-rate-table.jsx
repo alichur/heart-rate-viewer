1ece450d31feabfe1f0318ca48430cb1b586dca7
Ali Churcher <churcher.ali@gmail.com>
2016-10-30 13:53:48 +0100

    Small refactor changed loop to map

--- public/src/heart-rate-table.jsx
+++ public/src/heart-rate-table.jsx
@@ -4,10 +4,8 @@ const HeartRateRow = require('./heart-rate-row.jsx');
 class HeartRateTable extends React.Component {
   render () {
     const rows = [];
-    const length = this.props.heartData.length;
-    for (let i = 0; i < length; i += 1) {
-      rows.push(<HeartRateRow heartRecord={this.props.heartData[i]} />);
-    }
+    rows.push(this.props.heartData.map((record) => <HeartRateRow heartRecord={record} />));
+
 
     return (
       <div>
