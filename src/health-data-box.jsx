b6c2bf398810542aa9d2024b7e110060dcc1c9be
Ali Churcher <churcher.ali@gmail.com>
2017-02-15 14:11:11 +0100

    Changing non-stateful components into functions rather than es6 class.

--- src/health-data-box.jsx
+++ src/health-data-box.jsx
@@ -2,17 +2,15 @@ const React = require('react');
 const SearchBox = require('./search-box.jsx');
 const HeartRateTable = require('./heart-rate-table.jsx');
 
-class HealthDataBox extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>
-          Heart Rate
-        </h1>
-        <SearchBox/>
-        <HeartRateTable heartData={this.props.heartData}/>
-      </div>
-    );
-  }
+function HealthDataBox(props) {
+  return (
+    <div>
+      <h1>
+        Heart Rate
+      </h1>
+      <SearchBox/>
+      <HeartRateTable heartData={this.props.heartData}/>
+    </div>
+  );
 }
 module.exports = HealthDataBox;
--- src/heart-rate-row.jsx
+++ src/heart-rate-row.jsx
@@ -1,13 +1,12 @@
 const React = require('react');
-class HeartRateRow extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.heartRecord.time}
-        {'\u00A0'}
-        {this.props.heartRecord.BPM}
-      </div>
-    );
-  }
+
+function HeartRateRow(props) {
+  return (
+    <div>
+      {this.props.heartRecord.time}
+      {'\u00A0'}
+      {this.props.heartRecord.BPM}
+    </div>
+  );
 }
 module.exports = HeartRateRow;
