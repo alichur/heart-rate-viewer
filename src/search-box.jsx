12be3df3d25c779597588b8ab2abc43045315a19
Ali Churcher <churcher.ali@gmail.com>
2017-02-16 13:08:08 +0100

    Added state to select

--- src/search-box.jsx
+++ src/search-box.jsx
@@ -1,13 +1,19 @@
 const React = require('react');
 
 class SearchBox extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 'minutes'
+    }
+  }
   render() {
     return (
       <form>
         <label>
           Choose your level of detail
         </label>
-        <select>
+        <select value={this.state.value}>
           <option value='seconds'>Seconds</option>
           <option value='minutes'>Minutes</option>
         </select>
