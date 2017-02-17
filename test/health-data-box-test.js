d1641ef629e069f1dc9e7f28974d370c11ad630c
Ali Churcher <churcher.ali@gmail.com>
2017-02-17 23:51:21 +0100

    More Tests Default props in select, state updates in health data box

---
+++ test/health-data-box-test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import HealthDataBox from '../src/health-data-box.jsx'
+import {heartRateSeconds, heartRateMinute} from '../src/dummyData.js'
+
+describe('Health Data Box', () =>  {
+  it('should update state to the selected time units', () => {
+    const wrapper = mount(<HealthDataBox />);
+    wrapper.instance().handleTimeUnitChange('myNewUnits');
+    expect(wrapper.state('value')).to.be.equal('myNewUnits');
+  })
+
+  it('should update state with the data for the selected time units: seconds', () => {
+    const wrapper = mount(<HealthDataBox />);
+    wrapper.instance().handleTimeUnitChange('seconds');
+    expect(wrapper.state('heartData')).to.be.equal(heartRateSeconds);
+  })
+
+  it('should update state with the data for the selected time units: minutes', () => {
+    const wrapper = mount(<HealthDataBox />);
+    wrapper.instance().handleTimeUnitChange('minutes');
+    expect(wrapper.state('heartData')).to.be.equal(heartRateMinute);
+  })
+});
--- test/selection-box-test.js
+++ test/selection-box-test.js
@@ -2,6 +2,17 @@ import React from 'react';
 import SelectionBox from '../src/selection-box';
 
 describe('Selection Box', () => {
+  it('should have props for starting value and change handler', function () {
+    const wrapper = shallow(<SelectionBox/>);
+    expect(wrapper.props().value).to.be.defined;
+    expect(wrapper.props().handleChange).to.be.defined;
+  });
+
+  it('should have a default value set from props', function () {
+    const wrapper = shallow(<SelectionBox value={'seconds'}/>);
+    expect(wrapper.find('select').value).to.equal(wrapper.props().value);
+  });
+
   it('should trigger an update when the user makes a selection', () => {
     const handleChange = spy();
     const wrapper = shallow(<SelectionBox handleChange={handleChange}/>);
