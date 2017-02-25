c5f879fe881979670cb78fa28974ccddbb335797
Ali Churcher <churcher.ali@gmail.com>
2017-02-25 20:14:54 +0100

    Added browser history instead of hashHistory for cleaner urls Added a catch all to server to redirect to index.

--- server.js
+++ server.js
@@ -2,5 +2,11 @@ var express = require('express');
 
 app = express();
 app.use(express.static('public'));
+
+// Always return the main index.html so react-router urls are still served.
+app.get('*',function (req, res) {
+        res.redirect('/');
+    });
+    
 app.listen(8080);
 console.log('server running on port 8080');
