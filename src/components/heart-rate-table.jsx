674139507d0c27780e91c465cbab0125d4c45e04
Ali Churcher <churcher.ali@gmail.com>
2017-02-28 19:33:08 +0100

    Refactor selection box into dumb component

--- src/components/heart-rate-table.jsx
+++ src/components/heart-rate-table.jsx
@@ -3,7 +3,6 @@ import HeartRateRow from './heart-rate-row.jsx';
 
 function HeartRateTable(props) {
   const rows = [];
-  debugger;
   if (Array.isArray(props.heartData)) {
     rows.push(props.heartData.map((record) => <HeartRateRow key={record.time} heartRecord={record}/>));
   }
--- src/components/selection-box.jsx
+++ src/components/selection-box.jsx
@@ -1,28 +1,20 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 
-class SelectionBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this)
-  }
-  handleChange(event) {
-    this.props.handleChange(event.target.value);
-  }
-  render() {
-    var optionList = [];
-    this.props.options.forEach((option) => {
-        optionList.push(<option key={option.key} value={option.key}>{option.value}</option>);
-      });
-    return (
-      <form>
-        <label>
-          Choose your level of detail
-        </label>
-        <select className="detail-selector" value={this.props.value} onChange={this.handleChange}>
-          {optionList}
-        </select>
-      </form>
-    );
-  }
+function SelectionBox (props) {
+  let optionList = props.options.map((option) => <option key={option.key} value={option.key}>{option.value}</option>);
+  return (
+    <form>
+      <select className='detail-selector' value={props.value} onChange={(e) => props.handleChange(e.target.value)}>
+        {optionList}
+      </select>
+    </form>
+  );
 }
+
+SelectionBox.propTypes = {
+  value: PropTypes.string,
+  handleChange: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired
+};
+
 module.exports = SelectionBox;
